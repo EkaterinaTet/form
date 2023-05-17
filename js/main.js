7010e4d924ada8f485f5bb54dbda635d30199222
Ekaterina Tetiukhina <ekaterina.psychologist@mail.ru>
2023-05-17 12:24:50 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -116,7 +116,7 @@ document.addEventListener("DOMContentLoaded", function () {
     };
     reader.onerror = function (e) {
       //если будет какая то ошибка, то пользователь получит сообщение об ошибке.
-      alert("Ошибка");
+      alert("Произошла Ошибка");
     };
     reader.readAsDataURL(file);
   }
